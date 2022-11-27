f1da9bc1a29c42abcb0464db40609d54c9e93967
Hayri Can Gundogdu <karhanli34@outlook.com>
2022-11-27 21:50:35 +0300

    deneme

--- helloWorldWithCamp/src/helloWorldWithCamp/helloWorld.java
+++ helloWorldWithCamp/src/helloWorldWithCamp/helloWorld.java
@@ -4,7 +4,18 @@ public class helloWorld {
 
 	public static void main(String[] args) {
 		
-
+		//Patika.dev/java101 ders soruları:
+		//Aşağıdaki programın çıktısı nedir ?
+		//byte b = 0;
+		//int a = b++;
+		//System.out.println(a);
+		
+		//Eğer m = −12 ve n = −5, ise m%n işleminin sonucu ne olur?
+		int m = -12;
+		int n = -5;
+		int sonuc = m%n;
+			System.out.println(sonuc);
+			
 	}
 
 }
