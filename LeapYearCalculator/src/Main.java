960687c0d8afb01c2327d41c5417f5bea7886bbe
Hayri Can Gundogdu <karhanli34@outlook.com>
2022-12-20 11:32:21 +0300

    first commit

---
+++ LeapYearCalculator/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		int year;
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Yıl girin: ");
+		year = scanner.nextInt();
+		
+		if(year % 4 == 0) {
+			if (year % 100 == 0) {
+				if (year % 400 == 0) {
+					System.out.println(year + " bir artık yıl!");
+				}else {
+					System.out.println(year + " bir artık yıl değil!");
+				}
+			}else {
+				System.out.println(year + " bir artık yıl!");
+			}
+		}else {
+			System.out.println(year + " bir artık yıl değil!");
+		}
+		
+		
+		
+		
+	}
+
+}
